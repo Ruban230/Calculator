22c8bbb0ff27cd4fad9c7c27238da34aa07dea22
Ruban230 <103724420+Ruban230@users.noreply.github.com>
2023-08-30 17:49:49 +0500

    Add files via upload

---
+++ cal.ts
@@ -0,0 +1,65 @@
+import inquirer from 'inquirer';
+
+enum Operation {
+    ADD = 'Add',
+    SUBTRACT = 'Subtract',
+    MULTIPLY = 'Multiply',
+    DIVIDE = 'Divide',
+}
+
+function calculate(operator: Operation, num1: number, num2: number): number {
+    switch (operator) {
+        case Operation.ADD:
+            return num1 + num2;
+        case Operation.SUBTRACT:
+            return num1 - num2;
+        case Operation.MULTIPLY:
+            return num1 * num2;
+        case Operation.DIVIDE:
+            if (num2 !== 0) {
+                return num1 / num2;
+            } else {
+                throw new Error('Cannot divide by zero');
+            }
+        default:
+            throw new Error('Invalid operator');
+    }
+}
+
+async function runCalculator() {
+    const questions = [
+        {
+            type: 'list',
+            name: 'operator',
+            message: 'Select an operation:',
+            choices: Object.values(Operation),
+        },
+        {
+            type: 'input',
+            name: 'num1',
+            message: 'Enter the first number:',
+            validate: (value: string) => !isNaN(parseFloat(value)) || 'Please enter a valid number',
+        },
+        {
+            type: 'input',
+            name: 'num2',
+            message: 'Enter the second number:',
+            validate: (value: string) => !isNaN(parseFloat(value)) || 'Please enter a valid number',
+        },
+    ];
+
+    const answers = await inquirer.prompt(questions) as {
+        operator: Operation,
+        num1: string,
+        num2: string,
+    };
+
+    const result = calculate(
+        answers.operator,
+        parseFloat(answers.num1),
+        parseFloat(answers.num2)
+    );
+    console.log(`Result: ${result}`);
+}
+
+runCalculator();
